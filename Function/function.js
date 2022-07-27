ac46ace17b7c7065071cf9de33c68d519ef5c025
BUSHRA RIAZ <90851345+BushraRiaz@users.noreply.github.com>
2022-07-27 06:29:49 -0700

    Add files via upload

---
+++ Function/function.js
@@ -0,0 +1,34 @@
+// function cookMagi(magi,pani,pan)
+// {
+  // // // // console.log('Your magi will be ready in ' +  magi*2 + ' minutes' + ' and ingredients used are:- ' + magi + ' Magi ' + pani+ ' cups of water' + ' using ' + pan + ' pan');
+// }
+// cookMagi(4, 8,1);
+
+
+// let bread1=prompt('Which bread would you like to have:- ');
+// let veggies1=prompt('What are your favourite veggies');
+// let sauce1=prompt('Which sauce would you like to have:-')
+// function makeSandwich(bread,veggies,sauce){
+  // let sandwich =bread+' bread ' + veggies + '  ' + sauce +'  sandwich';
+  // return sandwich;
+// }
+// let vegSandwich = makeSandwich(bread1, veggies1, sauce1);
+// console.log(vegSandwich);
+
+// let a=2;
+// b=4;
+// function sum(a,b){
+  // let c='Sum of a and b is '+ a+b;
+  // return c;
+// }
+// let c1= sum(2+4);
+// console.log(c1);
+
+function nameList(){
+  console.log('Bushi Inshi');
+}
+nameList();
+function number(x,y){
+  return(x/y);
+}
+console.log(number(50,10));
